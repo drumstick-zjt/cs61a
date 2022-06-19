665cb6d4d16d0d76732f00c0ebdb6d80b43388a5
drumstick-zjt <1938245082@qq.com>
2022-06-19 19:13:59 +0800

    finish hw06.

--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -1,13 +1,24 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (ascending? lst) 'YOUR-CODE-HERE)
+(define (ascending? lst) (cond ((null? lst) #t) ((null? (cdr lst)) #t) (else (if (> (car lst) (cadr lst)) #f (ascending? (cdr lst))))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) (cond 
+    ((and (not (null? lst1)) (not (null? lst2))) (append (cons (car lst1) (cons (car lst2) nil)) (interleave (cdr lst1) (cdr lst2))) )  
+    ((null? lst1) lst2) 
+    ((null? lst2) lst1)
+    ))
 
-(define (my-filter func lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst) (cond
+    ((null? lst) nil)
+    ((func (car lst)) (append (cons (car lst) nil) (my-filter func (cdr lst))))
+    (else (my-filter func (cdr lst)))
+    ))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst) (cond
+    ((null? lst) nil)
+    (else (append (cons (car lst) nil) (no-repeats (my-filter (lambda (x) (not (= (car lst) x))) (cdr lst)))))
+    ))
