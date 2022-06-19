e40c7e85dd970a1e70ee37baa3a034d53522add8
drumstick-zjt <1938245082@qq.com>
2022-06-19 16:26:40 +0800

    finish lab10.

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,9 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond ((< num1 num2) -1) ((= num1 num2) 0) (else 1)) )
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) (cond ((= exp 0) 1) ((= exp 1) base) (else (if (even? exp) (square (pow base (/ exp 2))) (* base (square (pow base (quotient exp 2))))))))
